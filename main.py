4fcbaa317e7497685e1aa5ed709158bde0d08d35
Daniel <59041306+Cinco45@users.noreply.github.com>
2022-08-18 01:01:02 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,4 @@
+# input() will get user input in the console
+# Then print() will print the word "hello" and the user input together or "concatenate it"
+#add a space to "hello " or else it will concatenate w/out space
+print("hello " + input("Tell me your name"))
